6ada9896837471f7861905f8b96b1c6861ff6d2e
Lawrance Shepstone <lawrance@collabcollective.com>
2021-04-17 07:23:12 +0200

    Makes configuration possible from the ENV

--- lib/snap_api_client/errors.rb
+++ lib/snap_api_client/errors.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 module SnapAPIClient
+  # General
+  class ConfigurationError < StandardError; end
+
+  # HTTP
   class HttpResponseError < StandardError; end
   class HttpBadRequestError < HttpResponseError; end
   class HttpUnauthorizedError < HttpResponseError; end
