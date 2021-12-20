fe973fe976848ae4517946b5b595b8163abf26b8
Lawrance Shepstone <lawrance@collabcollective.com>
2021-12-20 09:56:34 +0100

    Corrects a few minor Rubocop offences

--- lib/snap_api_client/resources/pull_request.rb
+++ lib/snap_api_client/resources/pull_request.rb
@@ -4,12 +4,12 @@ module SnapAPIClient
   module Resources
     class PullRequest < Base
       def create_import_request(repository_name, number)
-        transport.post("/pull_requests", {
-          pull_request: {
-            repository_name: repository_name,
-            number: number
-          }
-        })
+        transport.post('/pull_requests', {
+                         pull_request: {
+                           repository_name: repository_name,
+                           number: number
+                         }
+                       })
       end
     end
   end
